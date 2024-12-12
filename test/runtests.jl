45bc14ce6f52fd0dcf06db192130e9bee4edbace
Sofia Calgaro <sofiacal@login38.chn.perlmutter.nersc.gov>
2024-12-12 07:52:03 -0800

    added runtest

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using Test
+
+@info "Running tests with $(Base.Threads.nthreads()) Julia threads active."
+
+import Pkg
+import Logging
+Logging.global_logger(Logging.ConsoleLogger(stdout, Logging.Info))
+
+Test.@testset "Package ZeroNuFit" begin
+    include("io/test_all.jl")
+    include("statistics/test_all.jl")
+end
