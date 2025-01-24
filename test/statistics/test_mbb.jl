1515dae9b5bfdbcd8e637cc634b0301615de1a6b
Sofia Calgaro <sofiacal@login36.chn.perlmutter.nersc.gov>
2025-01-24 07:00:42 -0800

    fixes

--- test/statistics/test_mbb.jl
+++ test/statistics/test_mbb.jl
@@ -10,12 +10,20 @@ include("../../src/constants.jl")
 
 @testset "test_mbb" begin
 
-    @info "Testing function for evaluating mbb (function 'mbb' in src/mbb.jl)"
+    @info "Testing function for computing mbb limits (function 'mbb' in src/mbb.jl)"
 
     # frequentist half-life limit taken from GSTR-20-006
     gerda_T12 = 1.83
-    gerda_low_limit = ZeroNuFit.mbb(1 / gerda_T12 * 10, G = constants.phase_space, M = constants.nme_gerda_up)
-    gerda_upp_limit = ZeroNuFit.mbb(1 / gerda_T12 * 10, G = constants.phase_space, M = constants.nme_gerda_low)
+    gerda_low_limit = ZeroNuFit.mbb(
+        1 / gerda_T12 * 10,
+        G = constants.phase_space,
+        M = constants.nme_gerda_up,
+    )
+    gerda_upp_limit = ZeroNuFit.mbb(
+        1 / gerda_T12 * 10,
+        G = constants.phase_space,
+        M = constants.nme_gerda_low,
+    )
 
     expected_prl_low_limit = 79
     expected_prl_upp_limit = 180
